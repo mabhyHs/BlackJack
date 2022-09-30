6a53fe8a59d242884d6eae6c2f30d50c3988b4db
mabhyHs <m4huens@gmail.com>
2022-09-30 12:10:18 -0300

    feature(cardValue-ShowPlayerPoints)

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,13 @@ let deck = [];
 const cardsTypes = ["C", "D", "H", "S"];
 const specialCards = ["A", "J", "Q", "K"];
 
+let playerPoints = 0;
+let computerPoints = 0;
+
+//HTML
+const btnAskCard = document.querySelector("#btnAskCard");
+const showPointsSmalls = document.querySelectorAll("small");
+
 // crear nueva baraja
 const createDeck = () => {
   for (let i = 2; i <= 10; i++) {
@@ -14,7 +21,7 @@ const createDeck = () => {
       deck.push(special + type);
     }
   }
-  deck = _.shuffle(deck);
+  deck = _.shuffle(deck); //libreria para hacer una especie de random con las cartas
   console.log(deck);
 };
 
@@ -23,21 +30,26 @@ createDeck();
 // Tomar una carta
 const askCard = () => {
   if (deck.length === 0) {
-    throw "No hay más cartas en el deck";
+    throw "No hay más cartas en el deck"; //chequeo si no hay más cartas en el deck
   }
-  const card = deck.pop();
-  console.log(card);
+  const card = deck.pop(); //si hay cartas extraigo la última del array
   return card;
 };
 
 //askCard();
 
 // saber el valor de una carta
-
 const cardValue = (card) => {
   const value = card.substring(0, card.length - 1);
   return isNaN(value) ? (value === "A" ? 11 : 10) : value * 1;
 };
 
-const valor = cardValue("KD");
-console.log({ valor });
+const valor = cardValue(askCard());
+
+// Listeners
+//Necesito poder "Escuchar" cuando se presiona un botón
+btnAskCard.addEventListener("click", () => {
+  const card = askCard();
+  playerPoints = playerPoints + cardValue(card); //modifico los puntos del jugador a medida que va pidiendo más cartas
+  showPointsSmalls[0].innerText = playerPoints; //muestro los puntos en el html
+});
